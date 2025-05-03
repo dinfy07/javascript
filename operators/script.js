7ef476e37a976992c00ce9c37cb9adf11b1fef03
Beybit <eshimkul07@mail.ru>
2025-05-03 17:00:16 +0500

    operators

---
+++ operators/script.js
@@ -0,0 +1,31 @@
+// Task 1
+let a = 10
+let b = 5
+let c = 2
+summ = a + b + c
+difference = a + c - b
+multiple = a * b / c
+remainder = a % b
+
+// Task 2
+let x = 7
+console.log(x++ + ++x)
+console.log(x-- - --x)
+
+// Task 3
+summ1 = 4 + 5
+console.log("The result is " + summ1)
+
+// Task 4
+console.log(a > b && a > 0 && b > 0)
+console.log(c < 10 || c == 2)
+console.log((a / c) != 5)
+
+// Task 5
+console.log('10' == "10") // true
+console.log('10' === "10") // true я думаю потому что они одинаковый стринги
+
+// Task 6
+x = 13
+console.log((x % 2 == 0 || x % 3 == 0) && (x % 6 != 0))
+console.log(x < 10 && x > 5)
