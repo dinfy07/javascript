b0e97ae93792fdb315a2a1c66ff0b11c12bc37d3
Beybit <eshimkul07@mail.ru>
2025-05-03 15:13:48 +0500

    type of data

---
+++ type of data/script.js
@@ -0,0 +1,40 @@
+// Task 1
+let number = 1
+const string = "B"
+
+number = 2
+// string = "H" Эту часть мы не можем изменить потому что это консотанта
+
+// Task 2
+// я дцмаю потому что в больших программах ты можем декларировать переменную которое уже существует и из за этого может быть оишбка
+
+// Task 3
+let number2 = 53
+let string2 = "dsthuh"
+let boolean = false
+let null1 = null
+let defined
+let undefined = NaN
+console.log(typeof(number2))
+console.log(typeof(string2))
+console.log(typeof(boolean))
+console.log(typeof(null))
+console.log(typeof(defined))
+console.log(typeof(undefined))
+
+// Task 4
+let numberToString = 456
+console.log(String(numberToString))
+let stringToNumber = "fgresges"
+console.log(parseInt(stringToNumber))
+
+// Task 5
+let second = 6744
+second = "sdfohsdukr"
+console.log(second) // ну здесь динамичная это означает что ты можешь при обновление результата изменить тип данных
+
+// Task 6
+let name = prompt()
+let age = prompt()
+console.log(name)
+console.log(age)
