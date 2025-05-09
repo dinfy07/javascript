a2aa7327ba3d280c0b6a779a83dbdcc6059cf8e7
Beybit <eshimkul07@mail.ru>
2025-05-09 15:05:27 +0500

    events

---
+++ events/script.js
@@ -0,0 +1,55 @@
+// TAsk 2
+let clickMe = document.querySelector("#jsButton");
+clickMe.addEventListener("click", function () {
+    alert("Click me with JS!");
+})
+
+// TAsk 3
+let div = document.querySelector(".myDiv");
+div.addEventListener("mouseover", function () {
+    div.style.backgroundColor = "blue";
+})
+div.addEventListener("click", function () {
+    alert("Элемент нажат");
+})
+// TAsk 4
+let input = document.querySelector("#textInput");
+input.addEventListener("keydown", function (event) {
+    console.log(event.key);
+})
+// TAsk 5
+let link = document.querySelector("#myLink");
+link.addEventListener("click", function (event) {
+    event.preventDefault();
+    alert("Переход по ссылке отменен");
+})
+// TAsk 6
+let list = document.querySelectorAll("li");
+list.forEach(li => {
+    li.addEventListener("click", function (event) {
+        console.log(event.target.textContent);
+    })
+})
+// TAsk 7
+let code = document.querySelector("#keyboardInput")
+code.addEventListener("keydown", function(event){
+    console.log(event.keyCode)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
